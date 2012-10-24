07586c714f331e43deceefed0d5d9d8aeb5ed199
Aleksey Filippov <sarum9in@gmail.com>
2012-10-24 20:43:03 +0400

    bacs::single::callback: errors declaration.

--- bacs/system/single/src/include/bacs/single/callback.hpp
+++ bacs/system/single/src/include/bacs/single/callback.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "bacs/single/error.hpp"
+
 #include "bacs/single/api/pb/result.pb.h"
 
 #include "bunsan/factory_helper.hpp"
@@ -12,14 +14,31 @@
 
 namespace bacs{namespace single{namespace callback
 {
+    struct error: virtual single::error {};
+    struct serialization_error: virtual error {};
+
+    struct result_error: virtual error {};
+    struct result_serialization_error:
+        virtual result_error, virtual serialization_error {};
+
+    struct intermediate_error: virtual error {};
+    struct intermediate_serialization_error:
+        virtual intermediate_error, virtual serialization_error {};
+
     class result: private boost::noncopyable
-    BUNSAN_FACTORY_BEGIN(result, const std::vector<std::string> &arguments)
+    BUNSAN_FACTORY_BEGIN(result, const std::vector<std::string> &/*arguments*/)
+    public:
+        virtual ~result() {}
+
     public:
         virtual void call(const api::pb::result::Result &result)=0;
     BUNSAN_FACTORY_END(result)
 
     class intermediate: private boost::noncopyable
-    BUNSAN_FACTORY_BEGIN(intermediate, const std::vector<std::string> &arguments)
+    BUNSAN_FACTORY_BEGIN(intermediate, const std::vector<std::string> &/*arguments*/)
+    public:
+        virtual ~intermediate() {}
+
     public:
         virtual void call(const std::vector<std::string> &state)=0;
 
