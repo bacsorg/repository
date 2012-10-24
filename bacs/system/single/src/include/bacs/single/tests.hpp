37eaa44262875113e192ba8de30e81d592a53ae7
Aleksey Filippov <sarum9in@gmail.com>
2012-10-25 02:08:25 +0400

    bacs::single::tests became class.

--- bacs/system/single/src/include/bacs/single/tests.hpp
+++ bacs/system/single/src/include/bacs/single/tests.hpp
@@ -3,15 +3,27 @@
 #include <string>
 #include <unordered_set>
 
-namespace bacs{namespace single{namespace tests
+#include <boost/noncopyable.hpp>
+
+namespace bacs{namespace single
 {
     /// \note must be implemented in problem
-    void create(const std::string &test_id, const std::string &data_id,
-                const boost::filesystem::path &path);
+    class tests: private boost::noncopyable
+    {
+    public:
+        tests();
+        ~tests();
 
-    /// \note must be implemented in problem
-    std::unordered_set<std::string> data_set();
+        void create(const std::string &test_id, const std::string &data_id,
+                    const boost::filesystem::path &path);
 
-    /// \note must be implemented in problem
-    std::unordered_set<std::string> test_set();
-}}}
+        std::unordered_set<std::string> data_set();
+
+        std::unordered_set<std::string> test_set();
+
+    private:
+        class impl;
+
+        impl *pimpl;
+    };
+}}
