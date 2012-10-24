9b0e2d8fdd87fce2ac4072ce17e02a01f699528c
Aleksey Filippov <sarum9in@gmail.com>
2012-10-25 02:14:52 +0400

    bacs/system/driver/simple0/tests stub fix.

--- bacs/system/driver/simple0/tests/src/src/tests/simple0.cpp
+++ bacs/system/driver/simple0/tests/src/src/tests/simple0.cpp
@@ -2,17 +2,35 @@
 
 #include "bacs/single/tests.hpp"
 
+#include <boost/property_tree/ptree.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/fstream.hpp>
 
-namespace bacs{namespace single{namespace tests
+namespace bacs{namespace single
 {
-    void create(const std::string &test_id, const std::string &data_id,
-                const boost::filesystem::path &path)
+    class tests::impl
+    {
+        std::unordered_set<std::string> test_set, data_set, text_data_set;
+    };
+
+    tests::tests():
+        pimpl(new impl)
+    {
+        boost::property_tree::ptree tests_info;
+        // TODO load internal data
+    }
+
+    tests::~tests()
+    {
+        delete pimpl;
+    }
+
+    void tests::create(const std::string &test_id, const std::string &data_id,
+                       const boost::filesystem::path &path)
     {
         const boost::filesystem::path name = test_id + "." + data_id;
         const boost::filesystem::path src = "share/tests" / name;
-        if (boost::filesystem::exists("etc/tests/convert" / name))
+        if (pimpl->text_data_set.find(data_id) != pimpl->text-data_set.end())
         {
             boost::filesystem::ifstream fin(src, std::ios::binary);
             boost::filesystem::ofstream fout(path);
@@ -24,13 +42,13 @@ namespace bacs{namespace single{namespace tests
         }
     }
 
-    std::unordered_set<std::string> data_set()
+    std::unordered_set<std::string> tests::data_set()
     {
-        // TODO
+        return pimpl->data_set;
     }
 
-    std::unordered_set<std::string> test_set()
+    std::unordered_set<std::string> tests::test_set()
     {
-        // TODO
+        return pimpl->test_set;
     }
-}}}
+}}
