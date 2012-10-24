2806f8828192891bfc15fe7c7512105642f38d3f
Aleksey Filippov <sarum9in@gmail.com>
2012-10-25 02:34:02 +0400

    bacs/system/checker/call/in_out_hint fix.

--- bacs/system/checker/call/in_out_hint/src/src/checker/in_out_hint.cpp
+++ bacs/system/checker/call/in_out_hint/src/src/checker/in_out_hint.cpp
@@ -2,9 +2,12 @@
 
 #include <boost/assert.hpp>
 
-namespace bacs{namespace single{namespace checker
+namespace bacs{namespace single
 {
-    result check(const file_map &test_files, const file_map &solution_files)
+    checker::checker() {}
+    checker::~checker() {}
+
+    checker::result checker::check(const file_map &test_files, const file_map &solution_files)
     {
         const boost::filesystem::path in = test_files.at("in"),
             out = solution_files.at("stdout"), hint = test_files.at("out");
@@ -13,4 +16,4 @@ namespace bacs{namespace single{namespace checker
         // TODO
         return result_;
     }
-}}}
+}}
