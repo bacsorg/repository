48a9a30cea2074ec0c53b68118ecba347afeb249
Aleksey Filippov <sarum9in@gmail.com>
2012-10-26 02:31:14 +0400

    bacs/system/checker/call/std/strict/out_stdout bug fix.
    bacs::single::detail::checker::result declaration.

--- bacs/system/checker/call/std/strict/out_stdout/src/include/bacs/single/detail/checker.hpp
+++ bacs/system/checker/call/std/strict/out_stdout/src/include/bacs/single/detail/checker.hpp
@@ -4,6 +4,8 @@
 
 namespace bacs{namespace single{namespace detail{namespace checker
 {
+    typedef single::checker::result result;
+
     result::status_type equal(std::istream &out, std::istream &hint);
     result::status_type seek_eof(std::istream &in);
 }}}}
