d0edc32f69a442c788e90bb4cd113a9dcb0fc6be
Aleksey Filippov <sarum9in@gmail.com>
2012-10-26 01:50:27 +0400

    bacs/system/driver/simple0/tests implementation except conversion.

--- bacs/system/driver/simple0/tests/src/src/main.cpp
+++ bacs/system/driver/simple0/tests/src/src/main.cpp
@@ -1,64 +1,16 @@
 #include "bunsan/config.hpp"
+#include "bunsan/system_error.hpp"
 
-#if 0
+#include <unordered_set>
+
+#include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <boost/assert.hpp>
+
 #include <boost/archive/text_iarchive.hpp>
-
-    class tests::impl
-    {
-        std::unordered_set<std::string> test_set, data_set, text_data_set;
-    };
-
-    tests::tests():
-        pimpl(new impl)
-    {
-        boost::filesystem::ifstream fin("etc/tests");
-        if (fin.bad())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("open"));
-        {
-            boost::text_iarchive ia(fin);
-            ia >> test_set >> data_set >> text_data_set;
-        }
-        if (fin.bad())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("read"));
-        if (fin.bad())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("close"));
-    }
-
-    tests::~tests()
-    {
-        delete pimpl;
-    }
-
-    void tests::create(const std::string &test_id, const std::string &data_id,
-                       const boost::filesystem::path &path)
-    {
-        const boost::filesystem::path name = test_id + "." + data_id;
-        const boost::filesystem::path src = "share/tests" / name;
-        if (pimpl->text_data_set.find(data_id) != pimpl->text-data_set.end())
-        {
-            //boost::filesystem::ifstream fin(src, std::ios::binary);
-            //boost::filesystem::ofstream fout(path);
-            // FIXME transform line endings
-            boost::filesystem::copy_file(src, path);
-        }
-        else
-        {
-            boost::filesystem::copy_file(src, path);
-        }
-    }
-
-    std::unordered_set<std::string> tests::data_set()
-    {
-        return pimpl->data_set;
-    }
-
-    std::unordered_set<std::string> tests::test_set()
-    {
-        return pimpl->test_set;
-    }
-#endif
+#include <boost/archive/text_oarchive.hpp>
+#include "yandex/contest/serialization/unordered_set.hpp"
 
 namespace
 {
@@ -72,11 +24,44 @@ namespace
 
 int main(int argc, char *argv[])
 {
-    BOOST_ASSERT(argc >= 1 + 1);
-    const boost::filesystem::path dst = argv[1];
-    for (int i = 2; i < argc; ++i)
+    BOOST_ASSERT(argc >= 2 + 1);
+    std::unordered_set<std::string> test_set, data_set, text_data_set;
+    {
+        boost::filesystem::ifstream fin("etc/tests");
+        if (fin.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("open"));
+        {
+            boost::archive::text_iarchive ia(fin);
+            ia >> test_set >> data_set >> text_data_set;
+        }
+        if (fin.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("read"));
+        if (fin.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("close"));
+    }
+    {
+        boost::filesystem::ofstream fout(argv[1]);
+        if (fout.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("open"));
+        {
+            boost::archive::text_oarchive oa(fout);
+            oa << test_set << data_set;
+        }
+        if (fout.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("write"));
+        if (fout.bad())
+            BOOST_THROW_EXCEPTION(bunsan::system_error("close"));
+    }
+    const boost::filesystem::path dst_dir = argv[2];
+    for (int i = 3; i < argc; ++i)
     {
         const boost::filesystem::path test = argv[i];
-        convert(test, dst / test.filename());
+        const boost::filesystem::path dst = dst_dir / test.filename();
+        const std::string data_id = test.extension().string();
+        BOOST_ASSERT(!data_id.empty());
+        if (text_data_set.find(data_id.substr(1)) != text_data_set.end())
+            convert(test, dst);
+        else
+            boost::filesystem::copy(test, dst);
     }
 }
