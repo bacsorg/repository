38ecea99c2bd6012a3e84baade1207a369a865c0
Aleksey Filippov <sarum9in@gmail.com>
2012-10-29 01:04:59 +0400

    bacs::single::testing::test(/*test*/) bug fixes.

--- bacs/system/single/src/src/lib/testing_test.cpp
+++ bacs/system/single/src/src/lib/testing_test.cpp
@@ -1,9 +1,13 @@
 #include "bacs/single/testing.hpp"
+#include "bacs/single/error.hpp"
+#include "bacs/single/detail/process.hpp"
 #include "bacs/single/detail/file.hpp"
 
 #include "bacs/single/api/pb/resource.pb.h"
 
 #include "yandex/contest/invoker/All.hpp"
+
+#include <boost/filesystem/operations.hpp>
 
 namespace bacs{namespace single
 {
@@ -19,7 +23,7 @@ namespace bacs{namespace single
         // TODO
         const ProcessGroupPointer process_group = m_container->createProcessGroup();
         const ProcessPointer process;// = process_group->createProcess(); // TODO somehow with executable, arguments initialized
-        setup(settings.resource_limits(), process_group, process);
+        detail::process::setup(settings.resource_limits(), process_group, process);
         const boost::filesystem::path current_path = "/tmp/testiing";
         const unistd::access::Id owner{1000, 1000};
         boost::filesystem::create_directories(m_container->filesystem().keepInRoot(current_path));
@@ -28,24 +32,24 @@ namespace bacs{namespace single
         struct receive_type
         {
             boost::filesystem::path path;
-            api::pb::settings::Range range;
+            api::pb::settings::File::Range range;
         };
         std::vector<receive_type> receive;
-        for (const api::pb::settings::File &file: process)
+        for (const api::pb::settings::File &file: settings.files())
         {
             if (files.find(file.id()) != files.end())
                 BOOST_THROW_EXCEPTION(error() << error::message("Duplicate file ids."));
             const boost::filesystem::path location = files[file.id()] =
-                "/" / current_path / file::detail::to_path(file.path()).filename(); // note: strip to filename
+                "/" / current_path / detail::file::to_path(file.path()).filename(); // note: strip to filename
             const boost::filesystem::path path = m_container->filesystem().keepInRoot(location);
             if (file.has_init())
-                m_tests->create(test_id, file.init(), path);
+                m_tests.create(test_id, file.init(), path);
             else
                 detail::file::touch(path);
-            m_container->filesystem().setOwner(current_path, owner);
+            m_container->filesystem().setOwnerId(current_path, owner);
             m_container->filesystem().setMode(current_path, 0400);
-            m_container->fileststem().setOwner(location, owner);
-            m_container->filesystem().setMode(location, detail::file::mode(file.permissions()));
+            m_container->filesystem().setOwnerId(location, owner);
+            m_container->filesystem().setMode(location, detail::file::mode(file.permissions()) & 0700);
             if (file.has_receive())
                 receive.push_back({path, file.receive()});
         }
@@ -67,19 +71,19 @@ namespace bacs{namespace single
             const boost::filesystem::path path = iter->second;
             switch (redirection.stream())
             {
-            case api::pb::settings::Execution::STDIN:
+            case api::pb::settings::Execution::Redirection::STDIN:
                 process->setStream(0, File(path, AccessMode::READ_ONLY));
                 break;
-            case api::pb::settings::Execution::STDOUT:
+            case api::pb::settings::Execution::Redirection::STDOUT:
                 process->setStream(1, File(path, AccessMode::WRITE_ONLY));
                 break;
-            case api::pb::settings::Execution::STDERR:
+            case api::pb::settings::Execution::Redirection::STDERR:
                 process->setStream(2, File(path, AccessMode::WRITE_ONLY));
                 break;
             }
         }
         // Execute
-        const ProcessGroup::Result process_group_result = process_group->syncrhonizedCall();
+        const ProcessGroup::Result process_group_result = process_group->synchronizedCall();
         const Process::Result process_result = process->result();
         // fill result
         result.set_id(test_id);
@@ -89,7 +93,7 @@ namespace bacs{namespace single
         {
             file_map test_files, solution_files;
             // TODO initialize files
-            const checker::result checker_result = m_checker->check(test_files, solution_files);
+            const checker::result checker_result = m_checker.check(test_files, solution_files);
             // fill checker result
             api::pb::result::TestResult::Checking &checking = *result.mutable_checking();
             // TODO
