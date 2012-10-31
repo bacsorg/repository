088c95cb9ef425a8a31d9488081302f26fe51246
Aleksey Filippov <sarum9in@gmail.com>
2012-10-31 19:30:29 +0400

    bacs::single::builders::fpc implementation.

---
+++ bacs/system/single/src/src/lib/builders/fpc.cpp
@@ -0,0 +1,61 @@
+#include "fpc.hpp"
+
+#include <boost/regex.hpp>
+#include <boost/assert.hpp>
+
+namespace bacs{namespace single{namespace builders
+{
+    const bool fpc::factory_reg_hook = builder::register_new("fpc",
+        [](const std::vector<std::string> &arguments)
+        {
+            builder_ptr tmp(new fpc(arguments));
+            return tmp;
+        });
+
+    static const boost::regex positional("^[^=]+$"), key_value("^([^=]+)=(.*)$");
+
+    fpc::fpc(const std::vector<std::string> &arguments)
+    {
+        for (const std::string &arg: arguments)
+        {
+            boost::smatch match;
+            if (boost::regex_match(arg, match, positional))
+            {
+                BOOST_ASSERT(match.size() == 1);
+                m_flags.push_back("-" + arg);
+            }
+            else if (boost::regex_match(arg, match, key_value))
+            {
+                BOOST_ASSERT(match.size() == 2);
+                const std::string key = match[1].str(), value = match[2].str();
+                if (key == "optimize")
+                {
+                    m_flags.push_back("-O" + value);
+                }
+                else if (key == "lang")
+                {
+                    m_flags.push_back("-x");
+                    m_flags.push_back(value);
+                }
+                else
+                {
+                    BOOST_THROW_EXCEPTION(invalid_argument_error() <<
+                                          invalid_argument_error::argument(arg));
+                }
+            }
+            else
+            {
+                BOOST_THROW_EXCEPTION(invalid_argument_error() <<
+                                      invalid_argument_error::argument(arg));
+            }
+        }
+    }
+
+    ProcessPointer fpc::create_process(const ProcessGroupPointer &process_group,
+                                       const boost::filesystem::path &source_name)
+    {
+        const ProcessPointer process = process_group->createProcess("fpc");
+        process->setArguments(process->executable(), m_flags, source_name, "-o", "executable");
+        return process;
+    }
+}}}
---
+++ bacs/system/single/src/src/lib/builders/fpc.hpp
@@ -0,0 +1,20 @@
+#include "native_compilable.hpp"
+
+namespace bacs{namespace single{namespace builders
+{
+    class fpc: public native_compilable
+    {
+    public:
+        explicit fpc(const std::vector<std::string> &arguments);
+
+    protected:
+        ProcessPointer create_process(const ProcessGroupPointer &process_group,
+                                      const boost::filesystem::path &source_name) override;
+
+    private:
+        std::vector<std::string> m_flags;
+
+    private:
+        static const bool factory_reg_hook;
+    };
+}}}
