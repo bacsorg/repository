f3201bcbbc8bffc4b2038437e4a8be6d89000425
Aleksey Filippov <sarum9in@gmail.com>
2013-02-22 17:25:01 +0400

    bacs::single::builder::mono implementation.

---
+++ bacs/system/single/src/src/lib/builders/mono.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "interpretable.hpp"
+
+#include "bunsan/forward_constructor.hpp"
+
+#include <memory>
+
+namespace bacs{namespace single{namespace builders
+{
+    class mono: public interpretable
+    {
+    public:
+        explicit mono(const std::vector<std::string> &arguments);
+
+    protected:
+        name_type name(const std::string &source) override;
+
+        ProcessPointer create_process(const ProcessGroupPointer &process_group,
+                                      const name_type &name) override;
+
+        solution_ptr create_solution(const ContainerPointer &container,
+                                     bunsan::tempfile &&tmpdir,
+                                     const name_type &name) override;
+
+    private:
+        std::string m_lang;
+        std::vector<std::string> m_flags;
+
+    private:
+        static const bool factory_reg_hook;
+    };
+}}}
