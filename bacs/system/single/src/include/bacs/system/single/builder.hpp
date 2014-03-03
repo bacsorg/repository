bc8b3c86bf700499afeba7ed8db485b3644723a5
Aleksey Filippov <sarum9in@gmail.com>
2014-03-03 15:29:38 +0400

    bacs::system::single::solution style fixes.

--- bacs/system/single/src/include/bacs/system/single/builder.hpp
+++ bacs/system/single/src/include/bacs/system/single/builder.hpp
@@ -37,7 +37,10 @@ namespace bacs{namespace system{namespace single
             const yandex::contest::invoker::ProcessGroupPointer &process_group,
             const Iter &begin, const Iter &end)
         {
-            return create(process_group, yandex::contest::invoker::ProcessArguments(begin, end));
+            return create(
+                process_group,
+                yandex::contest::invoker::ProcessArguments(begin, end)
+            );
         }
     };
     typedef std::shared_ptr<solution> solution_ptr;
@@ -51,10 +54,11 @@ namespace bacs{namespace system{namespace single
         virtual ~builder() {}
 
         /// \return nullptr on fail
-        virtual solution_ptr build(const yandex::contest::invoker::ContainerPointer &container,
-                                   const yandex::contest::system::unistd::access::Id &owner_id,
-                                   const std::string &source,
-                                   const problem::single::ResourceLimits &resource_limits,
-                                   problem::single::result::BuildResult &result)=0;
+        virtual solution_ptr build(
+            const yandex::contest::invoker::ContainerPointer &container,
+            const yandex::contest::system::unistd::access::Id &owner_id,
+            const std::string &source,
+            const problem::single::ResourceLimits &resource_limits,
+            problem::single::result::BuildResult &result)=0;
     BUNSAN_FACTORY_END(builder)
 }}}
